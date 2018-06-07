d962828d0014c6cf771a4dc668ff7525e4b88b0e
JaredAhaza <jaredahazq@gmail.com>
2018-06-07 11:11:38 +0300

    updated project

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jared/Documents/MoringaSchool/Core/Django/The-Moringa-Tribune/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
